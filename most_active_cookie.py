899afb81f9bd1954e5851ce7e591ce77618e6e14
joeyhe01 <33046535+joeyhe01@users.noreply.github.com>
2021-10-08 13:49:52 -0500

    Add files via upload

---
+++ most_active_cookie.py
@@ -0,0 +1,36 @@
+import heapq
+import sys
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+parser.add_argument("-d")
+args = parser.parse_args()
+import csv
+
+dateDict = {}#dictionary contains dates of entries as keys, and a dictionary of occurences of each item for that day
+
+with open(args.file, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        date = row[1][:10]
+        if date not in dateDict:
+            dateDict[date] = {}
+        if row[0] not in dateDict[date]:
+            dateDict[date][row[0]] = 1
+        else:
+            dateDict[date][row[0]] += 1
+#once dictionary is constructed, use dictionary to find max occurences of item on a single day and store in res
+res = []
+max = max(dateDict[args.d].values()) #number of occurences of highest occurence item on given date
+for item in dateDict[args.d]:
+    if dateDict[args.d][item] == max:
+        res.append(item)
+#print res
+for val in res:
+    print(val)
+
+
+
+
+
